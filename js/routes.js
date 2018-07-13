4d2039ed138b38ac0d14a0adcf28a30c0ec2b14c
Megan Roberts <megmrob@gmail.com>
2018-07-12 20:24:07 -0500

    fixed home link

--- js/routes.js
+++ js/routes.js
@@ -5,7 +5,11 @@ const express     = require('express');
 const router      = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', {title: 'RobertDola Wedding 2019'});
+  res.render('index', {title: 'RoberDola Wedding 2019'});
+});
+
+router.get('/photos', (req, res) => {
+  res.render('photos', {title: 'Photos | RoberDola Wedding 2019'});
 });
 
 module.exports = router;
